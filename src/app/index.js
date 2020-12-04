841174bbab3e1e58d7e38f87f59f67ed7ab340a0
Adriana Saty <adri-saty@hotmail.com>
2020-12-03 22:00:02 -0300

    instalando o redux e preparando pastas

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import { Provider } from 'react-redux';
+import store from './store';
 class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={store}>
         Dashboard
-      </div>
+      </Provider>
     )
   }
 }
